47d0179f6d5df23580a7df34319a73a399e556a8
MD Raza <raza231198@gmail.com>
2021-02-14 16:53:48 +0000

    RMX1941: Bringup for TWRP
    Signed-off-by: MD Raza <raza231198@gmail.com>

--- omni_RMX1941.mk
+++ omni_RMX1941.mk
@@ -20,10 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/pb/config/common.mk)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
-    $(LOCAL_PATH)/prebuilt/dtb:dtb.img
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX1941
